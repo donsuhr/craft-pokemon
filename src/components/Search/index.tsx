7305439aa15f862a46c6789220a59bed50408379
Donald Suhr <git@donsuhr.com>
2020-11-13 17:11:50 -0800

    rename initalVal to value

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -5,20 +5,20 @@ import styles from './Search.module.scss';
 
 interface Props {
   label?: string; // eslint-disable-line react/require-default-props
-  initialVal?: string; // eslint-disable-line react/require-default-props
+  value?: string; // eslint-disable-line react/require-default-props
   name?: string; // eslint-disable-line react/require-default-props
   idSuffix?: string; // eslint-disable-line react/require-default-props
   onChange?: (x: any) => any; // eslint-disable-line react/require-default-props
 }
 
 export default function Search({
-  initialVal = '',
+  value = '',
   idSuffix = 'search',
   name = 'search',
   label = 'Search',
   onChange,
 }: Props) {
-  const [val, updateVal] = useState(initialVal);
+  const [val, updateVal] = useState(value);
   const textInput = useRef(null);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
