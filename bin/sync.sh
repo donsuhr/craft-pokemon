4d981f1053f4329c794f7484eb3e8750128e193a
Donald Suhr <git@donsuhr.com>
2020-12-25 10:36:57 -0800

    move project to cloudfront folder

--- bin/sync.sh
+++ bin/sync.sh
@@ -2,10 +2,14 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 dist="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../dist" && pwd )/"
 
-aws s3 sync ${dist} s3://donsuhr.com-projects/pokemon \
---profile ec2-user \
---exclude "screen1.jpg" \
---exclude "screen2.jpg" \
---exclude "thumb.jpg" \
---exclude "*.DS_Store*" \
---delete
+aws s3 sync ${dist} s3://donsuhr.com-pokemon \
+  --profile ec2-user \
+  --exclude "screen1.jpg" \
+  --exclude "screen2.jpg" \
+  --exclude "thumb.jpg" \
+  --exclude "*.DS_Store*" \
+  --delete
+
+AWS_PAGER="bat -l json" aws cloudfront create-invalidation \
+  --distribution-id EIC3A1EXAKPYU \
+  --paths "/index.html" "/sw.js"
