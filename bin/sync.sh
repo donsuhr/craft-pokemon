05f42202ab8d5e665528ef65af8fcd8191013765
Donald Suhr <git@donsuhr.com>
2020-12-29 12:56:15 -0800

    also ignore webp thumb

--- bin/sync.sh
+++ bin/sync.sh
@@ -4,9 +4,10 @@ dist="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../dist" && pwd )/"
 
 aws s3 sync ${dist} s3://donsuhr.com-pokemon \
   --profile ec2-user \
-  --exclude "screen1.jpg" \
-  --exclude "screen2.jpg" \
-  --exclude "thumb.jpg" \
+  --exclude "screen1.png" \
+  --exclude "screen2.png" \
+  --exclude "thumb.png" \
+  --exclude "thumb.webp" \
   --exclude "*.DS_Store*" \
   --delete
 
